88e9dd377053dfb366d1dd0fc090bc0e3a41e9ad
Kevin Hernandez <37917262+Krizeon@users.noreply.github.com>
2020-10-07 21:18:38 -0700

    adjusting...

---
+++ maps/greenguy_walking1.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="greenguy_walking1" tilewidth="181" tileheight="208" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="181" height="208" source="../sprites/greenguy_walking1.png"/>
+ </tile>
+</tileset>
